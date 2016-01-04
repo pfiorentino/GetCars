e072dd4f380c837ed747fcf6157d90ed2eb633da
Paul FIORENTINO <fiorentino.paulalain@gmail.com>
2016-01-04 15:21:26 +0100

    Empty message (alias PleinG de modifes)

--- watcher.php
+++ watcher.php
@@ -1,4 +1,47 @@
 <?php
+    function term_head($value = "") {
+        if (!empty($value)){
+            $value = " $value ";
+        }
+
+        $remaining_chars = (80-strlen($value))/2;
+
+        echo str_repeat("=", floor($remaining_chars));
+        echo $value;
+        echo str_repeat("=", ceil($remaining_chars))."\n";
+    }
+
+    function term_print($value = "", $center = false) {
+        $remaining_chars = 78-strlen($value);
+
+        if ($center){
+            echo "|".str_repeat(" ", floor($remaining_chars/2));
+            echo $value;
+            echo str_repeat(" ", ceil($remaining_chars/2))."|\n";
+        } else {
+            echo "| ".$value.str_repeat(" ", $remaining_chars-1)."|";
+        }
+    }
+
+    function term_array_print($values = array()) {
+        $cell_size = floor(78 / count($values));
+
+        echo "|";
+        foreach ($values as $key => $value) {
+            $remaining_chars = $cell_size-strlen($value);
+
+            echo " ".$value;
+
+            if ($key < count($values)-1){
+                echo str_repeat(" ", $remaining_chars-2);
+                echo "|";
+            } else {
+                echo str_repeat(" ", $remaining_chars-1+(78-$cell_size*count($values)));
+            }
+        }
+        echo "|\n";
+    }
+
     $dbh = new PDO('mysql:host=mysql.montpellier.epsi.fr;port=5206;dbname=cars', "cars_user", "cars34");
 
     $stop = false;
@@ -6,14 +49,42 @@
     while(!$stop) {
         $query = "SELECT COUNT(id) as tasks_number, status FROM tasks GROUP BY status";
         $results = (array)$dbh->query($query)->fetchAll();
+        $tasks_running_query = "SELECT * FROM tasks WHERE status = 'IS_PROCESSING'";
+        $tasks_running_results = (array)$dbh->query($tasks_running_query)->fetchAll();
         $cars_query = "SELECT COUNT(id) as cars_number FROM cars";
         $cars_results = (array)$dbh->query($cars_query)->fetchObject();
 
         system('clear');
+
+        term_head("TASKS");
+        term_print();
+
+        $temp_array = array();
         foreach ($results as $key => $value) {
-            echo $value['status'].": ".$value['tasks_number']."\n";
+            $temp_array[] = $value['status'].": ".$value['tasks_number'];
         }
-        echo "Cars: ".$cars_results['cars_number'];
+        term_array_print($temp_array);
+
+        if (count($tasks_running_results) > 0){
+            term_print();
+
+            foreach ($tasks_running_results as $key => $value) {
+                term_print($value["brand_label"]." - ".$value["model_label"]);
+            }
+        }
+        term_print();
+        term_head();
+
+        echo "\n\n";
+
+        term_head("CARS");
+        term_print();
+
+        term_print("Cars: ".$cars_results['cars_number']);
+
+        term_print();
+        term_head();
+        echo "\n";
 
         //print_r($results);
 
