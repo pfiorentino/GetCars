c2bd1b28f63ec42d7a16146866e8e2b2c570967a
Paul FIORENTINO <fiorentino.paulalain@gmail.com>
2016-01-05 01:14:23 +0100

    Empty message (alias PleinG de modifes)

--- watcher.php
+++ watcher.php
@@ -13,6 +13,7 @@
 
     function term_print($value = "", $center = false) {
         $remaining_chars = 78-strlen($value);
+        echo strlen($value);
         echo $remaining_chars;
 
         if ($center){
@@ -78,6 +79,7 @@
                 }
             }
         }
+
         term_print();
         term_head();
 
